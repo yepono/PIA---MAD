e428d7b983f31d332f7d8b193dc147ab6babad13
yepono <tonbarrz5@gmail.com>
2025-05-25 20:30:07 -0600

    Add project files.

---
+++ PIA MAD/Pantallas/Login.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace PIA_MAD.Pantallas
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Id_Usuario = new System.Windows.Forms.TextBox();
+            this.txt_Contraseña = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(165, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID Usuario";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txt_Id_Usuario
+            // 
+            this.txt_Id_Usuario.Location = new System.Drawing.Point(146, 108);
+            this.txt_Id_Usuario.Name = "txt_Id_Usuario";
+            this.txt_Id_Usuario.Size = new System.Drawing.Size(100, 20);
+            this.txt_Id_Usuario.TabIndex = 1;
+            this.txt_Id_Usuario.TextChanged += new System.EventHandler(this.txt_Id_Usuario_TextChanged);
+            // 
+            // txt_Contraseña
+            // 
+            this.txt_Contraseña.Location = new System.Drawing.Point(146, 190);
+            this.txt_Contraseña.Name = "txt_Contraseña";
+            this.txt_Contraseña.Size = new System.Drawing.Size(100, 20);
+            this.txt_Contraseña.TabIndex = 3;
+            this.txt_Contraseña.TextChanged += new System.EventHandler(this.txt_Contraseña_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(165, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Contraseña";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 362);
+            this.Controls.Add(this.txt_Contraseña);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_Id_Usuario);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Id_Usuario;
+        private System.Windows.Forms.TextBox txt_Contraseña;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ PIA MAD/Pantallas/Login.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PIA_MAD.Pantallas
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_Id_Usuario_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_Contraseña_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
